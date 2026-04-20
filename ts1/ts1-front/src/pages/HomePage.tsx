1ca5bbeddfce180e2e15c2c85d27b77464a347de
Raphaela <bmraphaela@gmail.com>
2026-04-20 19:21:56 -0300

    style: projeto funcionando com tailwind v4

--- ts1/ts1-front/src/pages/HomePage.tsx
+++ ts1/ts1-front/src/pages/HomePage.tsx
@@ -2,25 +2,24 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
     return (
-        <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-950 via-gray-900 to-gray-800 text-white">
+        <main className="min-h-screen flex items-center justify-center">
             <div className="text-center max-w-md px-8 py-10 rounded-2xl bg-white/5 backdrop-blur-md border border-white/10 shadow-xl">
 
                 <h1 className="text-4xl font-bold tracking-wide">
                     PORTAL SPRB-BD
                 </h1>
-
                 <p className="text-gray-400 mt-3 text-sm">
                     Controle Satelital
                 </p>
+
 
                 {/* BOTÕES */}
                 <div className="mt-8 flex flex-col gap-4">
 
                     <Link
                         to="/login"
-                        className="w-full py-3 rounded-xl bg-blue-600 hover:bg-blue-500 active:scale-[0.98] transition-all font-semibold shadow-md"
-                    >
-                        Entrar no sistema
+                        className="w-full py-3 rounded-xl bg-blue-600 hover:bg-blue-500 active:scale-[0.98] transition-all font-semibold shadow-md">
+                        Login
                     </Link>
 
                     <Link
