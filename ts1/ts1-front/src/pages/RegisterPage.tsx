ea2b085438836badff8433251dc6ee5426c3748f
Raphaela <bmraphaela@gmail.com>
2026-04-20 14:22:51 -0300

    feat: ajustando telas

--- ts1/ts1-front/src/pages/RegisterPage.tsx
+++ ts1/ts1-front/src/pages/RegisterPage.tsx
@@ -1,11 +1,17 @@
-import LoginForm from "../features/Auth/LoginForm";
+import RegisterForm from "../features/Auth/RegisterForm";
 
 export default function RegisterPage() {
     return (
-        <main style={{ display: "grid", placeItems: "center", height: "100vh" }}>
-            <div>
-                <h1>Login</h1>
-                <LoginForm />
+        <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-950 via-gray-900 to-gray-800 text-white">
+            <div className="text-center max-w-md px-6 py-10 rounded-2xl bg-white/5 backdrop-blur-md border border-white/10 shadow-xl">
+
+                <h1 className="text-4xl font-bold tracking-wide">
+                    PORTAL SPRB-BD
+                </h1>
+
+                <h2>CADASTRO DE USUÁRIOS</h2>
+
+                <RegisterForm />
             </div>
         </main>
     );
