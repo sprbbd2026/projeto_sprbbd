d82b510cfdcbcced92d3ef6cccc7c1ca9724b970
Raphaela <bmraphaela@gmail.com>
2026-04-21 00:21:37 -0300

    feat: adicionando footer

--- ts1/ts1-front/src/pages/HomePage.tsx
+++ ts1/ts1-front/src/pages/HomePage.tsx
@@ -2,41 +2,41 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
     return (
-        <main className="min-h-screen flex items-center justify-center">
-            <div className="text-center max-w-md px-8 py-10 rounded-2xl bg-white/5 backdrop-blur-md border border-white/10 shadow-xl">
+        <div>
 
-                <h1 className="text-4xl font-bold tracking-wide">
-                    PORTAL SPRB-BD
-                </h1>
-                <p className="text-gray-400 mt-3 text-sm">
-                    Controle Satelital
-                </p>
+            <main className="flex items-center justify-center">
+                <div className="text-center">
 
+                    <h1 className="text-4xl font-bold">
+                        PORTAL SPRB-BD
+                    </h1>
 
-                {/* BOTÕES */}
-                <div className="mt-8 flex flex-col gap-4">
+                    <p className="text-gray-400 mt-3 text-sm">
+                        Controle Satelital
+                    </p>
 
-                    <Link
-                        to="/login"
-                        className="w-full py-3 rounded-xl  bg-[var(--accent)] hover:bg-[var(--text-h)]  active:scale-[0.98] transition-all font-semibold shadow-md">
-                        Login
-                    </Link>
+                    <div className="mt-8 flex flex-col gap-4">
+                        <Link
+                            to="/login"
+                            className="w-full py-3 rounded-xl bg-[var(--accent)] hover:bg-[var(--text-h)] active:scale-[0.98] transition-all font-semibold shadow-md"
+                        >
+                            Login
+                        </Link>
 
-                    <Link
-                        to="/register"
-                        className="w-full py-3 rounded-xl bg-white/10 hover:bg-white/20 active:scale-[0.98] transition-all font-semibold border border-white/10"
-                    >
-                        Criar conta
-                    </Link>
+                        <Link
+                            to="/register"
+                            className="w-full py-3 rounded-xl bg-white/10 hover:bg-white/20 active:scale-[0.98] transition-all font-semibold border border-white/10"
+                        >
+                            Criar conta
+                        </Link>
+                    </div>
 
-
+                    <p className="text-xs text-gray-500 mt-6">
+                        Acesso restrito a usuários autorizados
+                    </p>
 
                 </div>
-
-                <p className="text-xs text-gray-500 mt-6">
-                    Acesso restrito a usuários autorizados
-                </p>
-            </div>
-        </main>
+            </main>
+        </div>
     );
 }
--- ts1/ts1-front/src/pages/RegisterPage.tsx
+++ ts1/ts1-front/src/pages/RegisterPage.tsx
@@ -2,8 +2,8 @@ import RegisterForm from "../features/Auth/RegisterForm";
 
 export default function RegisterPage() {
     return (
-        <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-950 via-gray-900 to-gray-800 text-white">
-            <div className="text-center max-w-md px-6 py-10 rounded-2xl bg-white/5 backdrop-blur-md border border-white/10 shadow-xl">
+        <main className="min-h-screen flex items-center justify-center">
+            <div className="text-center max-w-md rounded-2xl">
 
                 <h1 className="text-4xl font-bold tracking-wide">
                     PORTAL SPRB-BD
@@ -14,5 +14,6 @@ export default function RegisterPage() {
                 <RegisterForm />
             </div>
         </main>
+
     );
 }
