357851a8c0ae583447657ddd8837154db232cf22
Raphaela <bmraphaela@gmail.com>
2026-04-21 00:28:35 -0300

    feaT: iniciando registro com campos do mer

--- ts1/ts1-front/src/components/Footer.tsx
+++ ts1/ts1-front/src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <div className="flex items-center justify-center">
 
-            <p className="text-xs text-gray-500 mt-6">
+            <p className="text-xs text-gray-500 mt-8">
                 © 2026 SPRB-BD
             </p>
         </div>
