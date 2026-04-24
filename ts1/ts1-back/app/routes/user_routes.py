efd8994f48ea7222ceb0b02c17fdcf6476f97751
Elias Jr. <elias.junior@fcamara.com.br.>
2026-04-24 13:05:44 -0300

    feat: add register user

---
+++ ts1/ts1-back/app/routes/user_routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.db.database import get_db
+from app.schemas.user_schema import UserCreateRequest, UserResponse
+from app.services.user_service import create_user
+
+router = APIRouter()
+
+
+@router.post("/register", response_model=UserResponse, status_code=201)
+def register(data: UserCreateRequest, db: Session = Depends(get_db)):
+    return create_user(db, data)
