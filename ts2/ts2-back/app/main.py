c3bc2735691280758b2f5581cdc968e9bd75343f
saviovnn <savioviannasilva@hotmail.com>
2026-05-20 20:02:20 -0300

    feat: habilitando middleware CORS para todas as origens no aplicativo FastAPI

--- ts2/ts2-back/app/main.py
+++ ts2/ts2-back/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.auth_routes import router as auth_router
 from app.routes.user_routes import router as user_router
 from app.db.database import engine
@@ -10,6 +11,15 @@ app = FastAPI(
     docs_url="/docs",
     redoc_url="/redoc",
     openapi_url="/openapi.json"
+)
+
+# Configuração de CORS para permitir requisições do frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
