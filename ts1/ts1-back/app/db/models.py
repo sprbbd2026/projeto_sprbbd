0bff7cf82a42aa7bdc14c5cd6637afc4333dc670
TatiPasold <tatypasold@gmail.com>
2026-05-26 17:57:42 -0300

    feat: update models

--- ts1/ts1-back/app/db/models.py
+++ ts1/ts1-back/app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from app.db.database import Base
 
 
@@ -22,3 +23,36 @@ class Usuario(Base):
     usr_login = Column(String, unique=True)
     usr_senha_hash = Column(String)
     usr_status = Column(String, default="ativo")
+
+
+class Satelite(Base):
+    __tablename__ = "satelite"
+
+    id_satelite = Column(Integer, primary_key=True, index=True)
+    id_constelacao = Column(Integer, nullable=True)
+    relogio_interno_offset = Column(Float, default=0.0)
+    codigo_prn = Column(String, unique=True, index=True)
+    numero_svn = Column(Integer, nullable=True)
+    status = Column(String, default="ativo")
+
+    telemetrias = relationship("Telemetria", back_populates="satelite")
+
+
+class Telemetria(Base):
+    __tablename__ = "telemetria"
+
+    id_telemetria = Column(Integer, primary_key=True, index=True)
+    id_satelite = Column(Integer, ForeignKey(
+        "satelite.id_satelite"), nullable=False)
+
+    temperatura = Column(Float)
+    timestamp_registro = Column(DateTime)
+    orientacao = Column(String)
+    checksum = Column(String)
+    memoria = Column(Float)
+    energia = Column(Float)
+    bateria = Column(Float)
+    relogio = Column(String)
+    cpu = Column(Float)
+
+    satelite = relationship("Satelite", back_populates="telemetrias")
