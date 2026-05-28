cd8e336545b1d521480519faf5ee68ab6b22b819
Cesar Sales <csales@lassoinformatics.com>
2026-05-28 08:22:24 -0400

    Refatoração: Substituição de `Config` por `ConfigDict` nos schemas de Telemetria e Usuário

--- ts2/ts2-back/app/schemas/user_schema.py
+++ ts2/ts2-back/app/schemas/user_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 class UserBase(BaseModel):
     nome: str
@@ -16,5 +16,4 @@ class UserCreate(UserBase):
 class UserResponse(UserBase):
     id: int
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
